97507907a43378cd41d5d2b9fa8df1f5e07e86e3
PichDesuwa <pichbothun@gmail.com>
2026-05-15 14:48:49 +0700

    asf

--- Main.java
+++ Main.java
@@ -1,5 +1,8 @@
 public class Main{
     public static void main(String[] args) {
-        
+        Algorithm_V1 algorithm = new Algorithm_Lyza();
+        int[] sample = {1, 2, 3, 4, 5};
+        algorithm.sum(sample);
+        System.out.println("The sum is: " + algorithm.sum(sample));
     }
 }
