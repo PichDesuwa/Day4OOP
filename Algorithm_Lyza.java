65bbc6dfaed5ece1ff21772cc64888effde23559
mAiyc <lyzatouy@gmail.com>
2026-05-15 14:32:14 +0700

    dfgh

--- Algorithm_Lyza.java
+++ Algorithm_Lyza.java
@@ -1,23 +1,21 @@
-public class Algorithm_Lyza extends Algorithm_V1 {
+abstract class Algorithm_Lyza extends Algorithm_V1 {
     @Override
     public int sum(int[] input) {
-        int total = 0;
-        for (int num : input) {
-            total += num;
+        int sum = 0;
+        for (int i = 0; i < input.length; i++) {
+            sum = sum + input[i];
         }
-        return total;
+        return sum;
+    }
+    public static void main(String[] args) {
+        Algorithm_Lyza lyza = new Algorithm_Lyza();
+        int[] input = {1, 2, 3, 4, 5};
+        int result = lyza.sum(input);       
+    }
+    static void printResult(int result) {
+        System.out.println("The sum is: " + result);
     }
 
-    @Override
-    double average(int[] input) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'average'");
-    }
+    }  
 
-    @Override
-    int mean(int[] input) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'mean'");
-    }
-
-}
+ 
