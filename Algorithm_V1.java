f8aebf1efcb8b1e848477a40b95b82ed00c2c9df
PichDesuwa <pichbothun@gmail.com>
2026-05-15 13:40:19 +0700

    file

--- Algorithm_V1.java
+++ Algorithm_V1.java
@@ -1,5 +1,10 @@
-public class Algorithm_V1 {
-    public static void main(String[] args) {
-        
-    }
+abstract  class Algorithm_V1 {
+    //attribute
+    int[] sample = {1, 2, 3, 4, 5};
+
+    //method sum (lyza)
+    abstract int sum();
+
+    //method average (phanith)
+    abstract double average();
 }
