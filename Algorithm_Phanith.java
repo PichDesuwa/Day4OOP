2c3f94dc12d1cdc694e6b147948e883ebea82b84
SOPHANITH <yoongsophanith18@gmail.com>
2026-05-22 10:37:04 +0700

    Update Algorithm_Phanith.java
    update

--- Algorithm_Phanith.java
+++ Algorithm_Phanith.java
@@ -1,34 +1,32 @@
-public class Algorithm_Phanith extends Algorithm_V1{
+public class Algorithm_Phanith extends Algorithm_V1 {
 
     @Override
-    int sum(int[] input) {
+    int[] bubbleSort(int[] input) {
         // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'sum'");
+        throw new UnsupportedOperationException("Unimplemented method 'bubbleSort'");
     }
 
     @Override
-    double average(int[] input) {
-        int sum = 0;
-        for (int i = 0; i < input.length; i++) {
-            sum = sum + input[i];
-        }
-        double average = (double) sum / input.length;
-        return average;
-    }
+    int[] selectionSort(int[] input) {
 
-    public static void main(String[] args) {
-        Algorithm_Phanith phanith = new Algorithm_Phanith();
-        int[] input = {1, 2, 3, 4, 5};
-        double result = phanith.average(input);
-        System.out.println("The average is: " + result);
     }
-
 
     @Override
-    int mean(int[] input) {
+    int[] mergeSort(int[] input) {
         // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'mean'");
+        throw new UnsupportedOperationException("Unimplemented method 'mergeSort'");
     }
 
-    
-}
+    @Override
+    int[] heapSort(int[] input) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'heapSort'");
+    }
+
+    @Override
+    int[] insertionSort(int[] input) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'insertionSort'");
+    }
+    @
+} 
